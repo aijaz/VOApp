e0fddc497f00e7c2dd526abe9098c7153fa7fcc6
Aijaz A. Ansari <aijaz@aijaz.net>
2020-08-17 21:13:47 -0600

    Populate accessibilityElements
    Now. VoiceOver reads in the correct order

--- VOApp/ViewController.swift
+++ VOApp/ViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var randLabel: UILabel!
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.accessibilityElements = [headingLabel!, nameLabel!, button!, randLabel!]
     }
 
     @IBAction func genRand(_ sender: Any) {
